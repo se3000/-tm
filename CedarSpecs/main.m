008feea463a797617de0cfb18553729c48d2c91d
Steve Ellis <email@steveell.is>
2013-07-24 12:31:40 -0400

    Add Cedar

---
+++ CedarSpecs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CedarSpecs
+//
+//  Created by Steve Ellis on 7/24/13.
+//  Copyright (c) 2013 Steve Ellis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Cedar-iOS/Cedar-iOS.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+    }
+}
